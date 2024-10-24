b81f1349ac136d092a03d0656a9f37d5d4559a2c
QileHuang <159638935+QileHuang@users.noreply.github.com>
2024-10-24 11:42:19 -0400

    Add files via upload

---
+++ trigger-webhooks.ps1
@@ -0,0 +1,15 @@
+# Webhook-1
+Invoke-WebRequest -Uri "https://automation.hivespark.io/webhook/simulate-trigger1" -Method Post
+Write-Host "Triggered simulate-trigger-1"
+
+
+# Webhook-2
+Invoke-WebRequest -Uri "https://automation.hivespark.io/webhook/simulate-trigger2" -Method Post
+Write-Host "Triggered simulate-trigger-2"
+
+
+# Webhook-3
+Invoke-WebRequest -Uri "https://automation.hivespark.io/webhook/simulate-trigger3" -Method Post
+Write-Host "Triggered simulate-trigger-3"
+
+
